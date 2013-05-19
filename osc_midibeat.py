579d7ce7842307edd5fc1ff83ef18ddf370f3ce6
Christiane Ruetten <cr@23bit.net>
2013-05-19 04:01:09 +0200

    pygame.midi instead of pypm, UDP socket instead of pyliblo

--- osc_midibeat.py
+++ osc_midibeat.py
@@ -6,21 +6,22 @@
 #     - Add out / Output / Beat Phase Monitor
 #       - Assignment: Device Target
 
-import pypm
 import array
 import time
-import liblo
 import sys
+import socket
+import pygame
+import pygame.midi
 
-IpAddress = "127.0.0.1"
-UdpPort = 1234
+IpAddress = "10.0.0.1"
+UdpPort = 9005
 
 INPUT=0
 OUTPUT=1
 
 def device_list( dir ):
-	for dev in range( pypm.CountDevices() ):
-		interf,name,inp,outp,opened = pypm.GetDeviceInfo( dev )
+	for dev in range( pygame.midi.get_count() ):
+		interf,name,inp,outp,opened = pygame.midi.get_device_info( dev )
 		if( (dir == INPUT) & (inp == 1) | (dir == OUTPUT) & (outp == 1)):
 			print dev, name, " ",
 			if( inp == 1 ): print "(input) ",
@@ -28,35 +29,37 @@ def device_list( dir ):
 			if( opened == 1 ): print "(opened)"
 			else: print "(unopened)"
 		print
+	
 
 def osc_beat():
 	device_list( INPUT )
 	dev = int( raw_input( "MIDI input number: " ) )
-	midi_in = pypm.Input( dev )
-#	midi_in.SetFilter( pypm.FILT_ACTIVE | pypm.FILT_CLOCK )
+	midi_in = pygame.midi.Input( dev )
 	cntr = 0
 	prev = 0
 	while 1:
 		cntr += 1
-		while not midi_in.Poll(): pass
-		midi_data = midi_in.Read(1)
+		while not midi_in.poll(): pass
+		midi_data = midi_in.read(1)
 		#print "message is ", cntr, ": time ", midi_data[0][1],", ",
 		#print  midi_data[0][0][0], " ", midi_data[0][0][1], " ", midi_data[0][0][2], midi_data[0][0][3]
 		value = midi_data[0][0][2]
 		if value < prev:
 			time.sleep( 0.1 ) # poor girl's sync
-			liblo.send( target, "/baem", "clock" )
+			#liblo.send( target, "/baem", "clock" )
+			packet = "/1/baem\x00,\x00\x00\x00"
+			target.sendto( packet, ( IpAddress, UdpPort ) )
 			sys.stdout.write( "baem! " )
 			sys.stdout.flush()
 		prev = value
 	del midi_in
     
 try:
-	target = liblo.Address( IpAddress, UdpPort )
-except liblo.AddressError, err:
+	target = socket.socket( socket.AF_INET, socket.SOCK_DGRAM, 0 )
+	pygame.midi.init()
+except err:
 	print str( err )
 	sys.exit()
 
 osc_beat()
-pypm.Terminate()
 
